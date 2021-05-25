cb289b7bd3b70975814db71644dcad04f52c00fd
Sebastian Espin <sebasespinalejo@gmail.com>
2021-05-25 02:22:48 +0200

    SQL 1ST commit

---
+++ ADDING.sql
@@ -0,0 +1,45 @@
+/*INSERT INTO LOCATIONS(LOCATION_ID, ADRESS, PHONENUMBER, EMIAL)
+VALUES (0, 'NC Waxhaw Choke berry 2 28173', '0992742216', 'waxhaw@sixt.com');
+
+INSERT INTO LOCATIONS(LOCATION_ID, ADRESS, PHONENUMBER, EMIAL)
+VALUES (1, 'NC Charlotte black berry 10 28175', '099274654', 'charlotte@sixt.com');
+
+INSERT INTO LOCATIONS(LOCATION_ID, ADRESS, PHONENUMBER, EMIAL)
+VALUES (2, 'NC Raylight sour berry 80 29173', '0982742216', 'raylight@sixt.com');
+
+INSERT INTO LOCATIONS(LOCATION_ID, ADRESS, PHONENUMBER, EMIAL)
+VALUES (3, 'FL Fort Lauderdale acid berry 88 50173', '0982782219', 'Fort.Lauderdale@sixt.com');
+
+INSERT INTO LOCATIONS(LOCATION_ID, ADRESS, PHONENUMBER, EMIAL)
+VALUES (4, 'FL Fort Lauderdale aiport 01 50273', '0972782219', 'Fort.aiport@sixt.com');
+
+INSERT INTO LOCATIONS(LOCATION_ID, ADRESS, PHONENUMBER, EMIAL)
+VALUES (5, 'SC Charleston 12 50298', '0972711219', 'Charleston@sixt.com'); 
+
+INSERT INTO COSTUMER(ID, LastName, FirstName, Age, PHONENUMBER, EMIAL, DRIVERLISENCE, CITY)
+VALUES (0, 'Salas', 'Juan', '19', '088776655','salas111@fake.com','ABC12345',0);
+
+
+INSERT INTO COSTUMER(ID, LastName, FirstName, Age, PHONENUMBER, EMIAL, DRIVERLISENCE, CITY)
+VALUES (1, 'Salsa', 'Mario', '43', '088776654','salas112@fake.com','ABC12346',1);
+
+INSERT INTO COSTUMER(ID, LastName, FirstName, Age, PHONENUMBER, EMIAL, DRIVERLISENCE, CITY)
+VALUES (2, 'Salsaron', 'Mariano', '36', '088776653','salas113@fake.com','ABC12347',0);
+
+
+INSERT INTO COSTUMER(ID, LastName, FirstName, Age, PHONENUMBER, EMIAL, DRIVERLISENCE, CITY)
+VALUES (3, 'Salado', 'TOM', '60', '088776652','salas114@fake.com','ABC12348',3);
+
+INSERT INTO COSTUMER(ID, LastName, FirstName, Age, PHONENUMBER, EMIAL, DRIVERLISENCE, CITY)
+VALUES (4, 'Sal', 'TOMAS', '61', '088776652','salas114@fake.com','ABC12349',3);
+
+
+INSERT INTO EMPLOYEE(ID, LastName, FirstName, Age, PHONENUMBER, EMIAL, LOCATION_ID)
+VALUES (0, 'PAZ', 'TOMAS', '20', '445566770','pazt@sixt.com',0),
+(1, 'PATA', 'TOMY', '22', '445566771','patat@sixt.com',1),
+(2, 'PATATA', 'JUAN', '34', '445566772','patataj@sixt.com',2),
+(3, 'PATATA', 'SOL', '28', '445566773','patatas@sixt.com',3),
+(4, 'GUERRA', 'SOLEDAD', '28', '445566774','GUERRAS@sixt.com',4),
+(5, 'GUERRA', 'Juan', '49', '445566775','GUERRAj@sixt.com',5),
+(6, 'Anarquia', 'Mateo', '52', '445566776','anarquiam@sixt.com',0);
+*/
